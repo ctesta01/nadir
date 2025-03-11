c1cebda4891500b1f11b195b324798eb47db440a
Christian Testa <ctesta@hsph.harvard.edu>
2025-03-11 12:55:07 -0400

    Benchmarking results; add list_known_learners()

--- R/helpers.R
+++ R/helpers.R
@@ -38,7 +38,32 @@ mean_squared <- function(x) {
 #' @keywords internal
 #' @param x A numeric vector
 #' @importFrom stats rbinom
+#' @returns A vector of integer values
 stochastic_round <- function(x) {
   floor(x) + rbinom(n = length(x), prob = x %% 1, size = 1)
 }
 
+
+#' List Known Learners
+#'
+#' @param type One of 'any' or a supported outcome type in nadir including
+#' at least 'continuous', 'binary', 'multiclass', 'density'. See \code{?super_learner()}.
+#' @returns A character vector of functions that were automatically recognized as
+#' nadir learners with the prediction/outcome type given.
+#' @export
+#' @examples
+#' list_known_learners()
+#' list_known_learners('continuous')
+#' list_known_learners('binary')
+#' list_known_learners('density')
+#' list_known_learners('multiclass')
+list_known_learners <- function(type = 'any') {
+  ls_output <- c(ls(envir = .GlobalEnv),
+                 ls(envir = environment(nadir::super_learner)))
+
+  if (type == 'any') {
+    return(ls_output[sapply(ls_output, \(x) ! is.null(attr(get(x), 'sl_lnr_type')))])
+  } else if (type %in% nadir_supported_types) {
+    return(ls_output[sapply(ls_output, \(x) type %in% attr(get(x), 'sl_lnr_type'))])
+  }
+}
